bc3dfc0c16f6ecfd61d1af94c4aafa2756adc4de
Huong Dang <miley.dawn@codeimmersives.com>
2020-11-30 04:34:07 -0500

    create todo routes

--- routes/todo/todo.js
+++ routes/todo/todo.js
@@ -9,14 +9,9 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/create-todo', jwtChecker, createTodo)
-// // way 1 use query
-// router.get('/get-user-all-todo', getAllUserTodos)
-// way 2 user params. params is the part after the /
+
 router.get('/get-user-all-todo/:id', jwtChecker, getAllUserTodos)
 
-// //way 1: using 2 params
-// router.delete('/delete-todo/:userId/:todoId', deleteTodo)
-//way 2: using 2 body
 router.delete('/delete-todo', jwtChecker, deleteTodo)
 
 router.put('/update-todo', jwtChecker, updateTodo)
